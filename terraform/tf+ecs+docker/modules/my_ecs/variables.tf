bf885bf698946fccfb8346e497595cd903d78efc
bruno queiros <bqueiros@gmail.com>
2020-11-08 11:28:11 +0000

    add root module policies

--- terraform/tf+ecs+docker/modules/my_ecs/variables.tf
+++ terraform/tf+ecs+docker/modules/my_ecs/variables.tf
@@ -1,97 +1,82 @@
-variable "id" {}
-
-variable "name" {}
-
-variable "schedule" {
-  description = "If the instance is running on a Schedule. Defaults to empty/null"
-  default     = ""
+variable "cluster" {
+  default     = "default"
+  description = "The name of the ECS cluster"
 }
 
-variable "snapshot" {
-  description = "If the instance volume is to be taken snapshots. Defaults to empty/null"
-  default     = ""
+variable "environment" {
+  description = "The name of the environment"
 }
 
-variable "alias" {
-  default = []
+variable "instance_group" {
+  default     = "default"
+  description = "The name of the instances that you consider as a group"
+}
+
+variable "vpc_cidr" {
+  description = "VPC cidr block. Example: 10.0.0.0/16"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list
+  description = "List of private cidrs, for every avalibility zone you want you need one. Example: 10.0.0.0/24 and 10.0.1.0/24"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list
+  description = "List of public cidrs, for every avalibility zone you want you need one. Example: 10.0.0.0/24 and 10.0.1.0/24"
+}
+
+variable "load_balancers" {
+  type        = list
+  default     = []
+  description = "The load balancers to couple to the instances"
+}
+
+variable "availability_zones" {
+  type        = list
+  description = "List of avalibility zones you want. Example: eu-west-1a and eu-west-1b"
+}
+
+variable "max_size" {
+  description = "Maximum size of the nodes in the cluster"
+}
+
+variable "min_size" {
+  description = "Minimum size of the nodes in the cluster"
+}
+
+variable "desired_capacity" {
+  description = "The desired capacity of the cluster"
+}
+
+variable "key_name" {
+  description = "SSH key name to be used"
 }
 
 variable "instance_type" {
-  description = "The type of instance to start. Updates to this field will trigger a stop/start of the EC2 instance."
-  default     = "t3a.medium"
+  description = "AWS instance type to use"
 }
 
-variable "aws_route53_zone_id" {
-  description = "The Hosted Zone ID"
-  type    = string
+variable "ecs_aws_ami" {
+  description = "The AWS ami id to use for ECS"
 }
 
-variable "disks" {
-  default = []
-}
-
-variable "vpc_security_group_ids" {
-  type    = list(string)
-  default = ["sg-32876a5d", "sg-4ea34c21"]
-}
-
-variable "my_instance_ebs_optimized_default" {
-  type        = bool
-  description = "If the instance is ebs optimized, used whenever ebs_optimized is not specified for the instance. Defaults true"
-  default     = true
-}
-
-variable "my_route53_record_ttl" {
-  type    = string
-  default = "300"
-}
-
-variable "my_instance_disable_api_termination" {
-  type        = bool
-  description = "If the instance has api_termination disabled, used whenever api_termination is not specified for the instance. Defaults true"
-  default     = true
-}
-
-variable "my_instance_source_dest_check" {
-  type        = bool
-  description = "Controls if traffic is routed to the instance when the destination address does not match the instance. Used for NAT or VPNs. Defaults true"
-  default     = true
-}
-
-variable "my_instance_iam_instance_profile" {
-  type        = string
-  description = "IAM profile for the instance"
+variable "custom_userdata" {
   default     = ""
+  description = "Inject extra command in the instance template to be run on boot"
 }
 
-variable "my_key_name" {
-  type    = string
-  default = "tiagovarela"
+variable "ecs_config" {
+  default     = "echo '' > /etc/ecs/ecs.config"
+  description = "Specify ecs configuration or get it from S3. Example: aws s3 cp s3://some-bucket/ecs.config /etc/ecs/ecs.config"
 }
 
-data "template_file" "bootstrap" {
-  template = "${file("${path.module}/../bootstraps/bootstrap.sh")}"
-  vars = {
-    id = "${var.id}"
-  }
+variable "ecs_logging" {
+  default     = "[\"json-file\",\"awslogs\"]"
+  description = "Adding logging option to ECS that the Docker containers can use. It is possible to add fluentd as well"
 }
 
-data "aws_ami" "ubuntu_xenial" {
-  most_recent = true
-  owners      = ["aws-marketplace"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-*-amd64-server-*"]
-  }
-}
-
-data "aws_ami" "ubuntu_bionic" {
-  most_recent = true
-  owners      = ["aws-marketplace"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-*-amd64-server-*"]
-  }
+variable "cloudwatch_prefix" {
+  default     = ""
+  description = "If you want to avoid cloudwatch collision or you don't want to merge all logs to one log group specify a prefix"
 }
